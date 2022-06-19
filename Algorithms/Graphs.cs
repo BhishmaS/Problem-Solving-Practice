12509b5a5e29bdbf590ea1e22021638ea06e8dfa
Bhishma Sukhavasi <bhishma.sukhavasi@gmail.com>
2022-06-19 16:36:54 -0700

    included graphs union find pattern

---
+++ Algorithms/Graphs.cs
@@ -0,0 +1,45 @@
+﻿namespace Learning.Algorithms
+{
+    public static class GraphsDemo
+    {
+        public static void Test()
+        {
+            var unionFind = new UnionFind();
+        }
+    }
+
+    public class UnionFind
+    {
+        // O(alpha(N)) Where alpha(N) is inverse ackermann function.. explore
+        public bool Union(int[] parents, int[] sizes, int v1, int v2)
+        {
+            var p1 = this.GetParent(parents, v1);
+            var p2 = this.GetParent(parents, v2);
+
+            if (p1 == p2)
+            {
+                return false;
+            }
+            else if (sizes[p1] > sizes[p2])
+            {
+                sizes[p1] += sizes[p2];
+                parents[p2] = p1;
+            }
+            else
+            {
+                sizes[p2] += sizes[p1];
+                parents[p1] = p2;
+            }
+
+            return true;
+        }
+
+        private int GetParent(int[] parents, int v)
+        {
+            if (parents[v] == v)
+                return v;
+
+            return this.GetParent(parents, parents[v]);
+        }
+    }
+}
