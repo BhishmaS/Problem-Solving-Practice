ee80b9687afbbefd45c81deee6b01f66b0040da3
Bhishma Sukhavasi <bhishma.sukhavasi@gmail.com>
2022-06-19 16:33:30 -0700

    Threads test

---
+++ OOPS/Thread.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Learning.OOPS
+{
+    public class ThreadDemo
+    {
+        public static void Test()
+        {
+            var t = new Thread(new ThreadStart(ThreadProc));
+        }
+
+        public static void ThreadProc()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                // Yield the rest of the time slice.
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
